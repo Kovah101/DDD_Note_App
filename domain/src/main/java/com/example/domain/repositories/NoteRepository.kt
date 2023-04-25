2a30527a246b6026b2cde48fb842c6376b82e7ab
Elliot Thompson <elliotthompson101@gmail.com>
2023-04-25 16:25:56 +0100

    added Note and Note repository to domain layer

--- domain/src/main/java/com/example/domain/repositories/NoteRepository.kt
+++ domain/src/main/java/com/example/domain/repositories/NoteRepository.kt
@@ -3,9 +3,10 @@ package com.example.domain.repositories
 import com.example.domain.models.Note
 
 interface NoteRepository {
-    suspend fun add(note: Note)
+    suspend fun addNote(note: Note)
     suspend fun update(note: Note)
     suspend fun delete(note: Note)
+    suspend fun deleteAll()
     suspend fun getNotes(): List<Note>?
     suspend fun getNoteById(id: Long): Note?
 }
