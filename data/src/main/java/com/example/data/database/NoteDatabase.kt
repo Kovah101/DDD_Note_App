d1845e1fc6acccd1528a3a25ae35f428b50d15d8
Elliot Thompson <elliotthompson101@gmail.com>
2023-05-04 16:58:22 +0100

    added type converter for checklist item, added kotlin serialisation to imports

--- data/src/main/java/com/example/data/database/NoteDatabase.kt
+++ data/src/main/java/com/example/data/database/NoteDatabase.kt
@@ -2,10 +2,14 @@ package com.example.data.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
 import com.example.data.daos.NoteDao
 import com.example.data.entities.NoteEntity
+import com.example.data.typeconverters.ChecklistConverter
 
 @Database(entities = [NoteEntity::class], version = 1)
+@TypeConverters(ChecklistConverter::class)
 abstract class NoteDatabase : RoomDatabase() {
     abstract fun noteDao(): NoteDao
 }
